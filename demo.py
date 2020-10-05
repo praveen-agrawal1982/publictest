da6231b6127c38665b495f228771e0df8cd56aca
Praveen Agrawal <praveen.agrawal@philips.com>
2020-10-05 12:57:31 +0530

    demo file

--- demo.py
+++ demo.py
@@ -4,8 +4,7 @@ import sys
 import os
 print(sys.argv[1])
 print(sys.argv[2])
-print(sys.argv[3])
-print(os.getenv('name'))
+print(os.getenv('lastName'))
 print(os.getenv('CDAL-host'))
 
 f= open(os.getenv('MODEL_PATH') + "/model.pl","w+")
