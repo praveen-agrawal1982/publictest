9b499ebd0e7b2ef7bafb609525a02f9c7a63ef0b
praveen-agrawal82 <66623814+praveen-agrawal82@users.noreply.github.com>
2020-10-05 13:31:26 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -6,7 +6,7 @@ print(sys.argv[1])
 print(sys.argv[2])
 print(sys.argv[3])
 print(os.getenv('name'))
-print(os.getenv('CDAL-host'))
+print(os.getenv('CDAL_host'))
 
 f= open(os.getenv('MODEL_PATH') + "/model.pl","w+")
 f.write("This is demo file")
