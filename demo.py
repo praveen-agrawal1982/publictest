7fc2a61abfcd4c84e372205168ce6896fafa4258
praveen-agrawal82 <66623814+praveen-agrawal82@users.noreply.github.com>
2022-03-05 06:11:11 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,12 +1,12 @@
 print("Greetings")
-  
+import time  
 import sys
 import os
 print(sys.argv[1])
 print(sys.argv[2])
 print(os.getenv('lastName'))
 print(os.getenv('CDAL_host'))
-
+time.sleep(259200)
 f= open(os.getenv('MODEL_PATH') + "/model.pl","w+")
 f.write("This is demo file")
 f.close()
